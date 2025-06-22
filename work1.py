d47a5b3dd78e1b2523db6c33685e2027fdd947fa
LesyaKot <mykolakotkov1@gmail.com>
2025-06-22 11:45:29 +0300

    homework3

---
+++ work1.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+t = turtle.Turtle()
+t.speed(1)
+
+
+t.penup()
+t.goto(-100, 0)  
+t.pendown()
+t.forward(200) 
+
+
+t.penup()
+t.goto(-100, -50)  
+t.pendown()
+t.forward(200)
---
+++ work2.py
@@ -0,0 +1,16 @@
+import turtle
+
+
+t = turtle.Turtle()
+t.speed(1) 
+
+
+length = 200  
+width = 100   
+
+
+for _ in range(2):
+    t.forward(length)   
+    t.right(90)         
+    t.forward(width)    
+    t.right(90)        
